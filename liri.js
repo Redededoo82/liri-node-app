1006cbc7a211476a1328b6f3aa6cd17c7482464d
AARON M <asmarcks@gmail.com>
2019-10-03 19:08:04 -0500

    initial file set up complete.

---
+++ liri.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+// get user input
+// create a variable for the command
+// create a variable for the data
+
+// if user types concert this 
+    // do something
+// if the user types spotify this 
+    // do something
+// if user types movie this 
+    // do something
+// if the user types do what it says 
+    // do something
