21b9b7fa47da960d24d425e7bb4b01c6e1a565b5
AARON M <asmarcks@gmail.com>
2019-10-03 23:07:59 -0500

    concert function complete, axios get request, data loop and logging results

--- liri.js
+++ liri.js
@@ -1,17 +1,35 @@
 require("dotenv").config();
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+var fs = require('fs');
+// var inquirer = require('inquirer');
+var axios = require('axios');
+var moment = require('moment');
 
-// get user input
-// create a variable for the command
-// create a variable for the data
 
 // if user types concert this 
-    // do something
-// if the user types spotify this 
-    // do something
-// if user types movie this 
-    // do something
-// if the user types do what it says 
-    // do something
+// do something
+
+switch (process.argv[2]) {
+
+    case "concert-this":
+        
+        var artist = process.argv[3];
+        concert();
+
+        function concert() {
+            var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+            axios.get(URL)
+            .then(function(results){
+                console.log('UPCOMING SHOWS!:');
+                for(var i = 0; i < 10; i++) {
+                    console.log("\n" + results.data[i].venue.name);
+                    console.log(results.data[i].venue.city + "," + results.data[i].venue.region);
+                    console.log(moment(results.data[i].datetime));
+                }
+
+            })
+        }
+        
+}
